c0c42b3c21d6ae83e3204a1777138a6c9d9b28d4
souviksaha05 <115524971+souviksaha05@users.noreply.github.com>
2024-06-27 01:04:55 +0530

    updated 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import AppRoutes from "./AppRoutes";
 import Header from "./components/Header/Header";
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
     <Header />
+    <AppRoutes />
     </>
   );
 
---
+++ frontend/src/data.js
@@ -0,0 +1,75 @@
+export const sample_foods = [
+    {
+        id: '1',
+        name: 'Pizza',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins:['italy'],
+        stars: 4.5,
+        imageUrl: 'food-1.jpg',
+        tags: ['Fastfood','Pizza','Lunch'],
+    },
+
+    {
+        id: '2',
+        name: 'Meatball',
+        cookTime: '10-20',
+        price: 20,
+        favorite: true,
+        origins:['italy'],
+        stars: 4.5,
+        imageUrl: 'food-2.jpg',
+        tags: ['Fastfood','Pizza','Lunch'],
+    },
+
+    {
+        id: '3',
+        name: 'Hamburger',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins:['italy'],
+        stars: 4.5,
+        imageUrl: 'food-3.jpg',
+        tags: ['Fastfood','Pizza','Lunch'],
+    },
+
+    {
+        id: '4',
+        name: 'Fried Potatoes',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins:['italy'],
+        stars: 4.5,
+        imageUrl: 'food-4.jpg',
+        tags: ['Fastfood','Pizza','Lunch'],
+    },
+
+    {
+    
+        id: '5',
+        name: 'Chicken Soup',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins:['italy'],
+        stars: 4.5,
+        imageUrl: 'food-5.jpg',
+        tags: ['Fastfood','Pizza','Lunch'],
+    },
+
+    {
+        id: '6',
+        name: ' Vegetables Pizza',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins:['italy'],
+        stars: 4.5,
+        imageUrl: 'food-6.jpg',
+        tags: ['Fastfood','Pizza','Lunch'],
+    },
+
+]
